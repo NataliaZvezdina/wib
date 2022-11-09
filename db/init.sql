f2b122b0e256a1306e078d5afcddaca8c43f7cc7
NataliaZvezdina <natalja.zvezdina@yandex.by>
2022-11-09 22:28:10 +0300

    add init script for tables, form db service in docker-compose.yml

---
+++ db/init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE IF NOT EXISTS users (
+	user_id SERIAL PRIMARY KEY,
+	age INT
+);
+
+CREATE TABLE IF NOT EXISTS items (
+	item_id SERIAL PRIMARY KEY,
+	price NUMERIC(5, 2)
+);
+
+CREATE TABLE IF NOT EXISTS purchases (
+    purchase_id SERIAL PRIMARY KEY,
+    user_id INT NOT NULL,
+    item_id INT NOT NULL,
+    date TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users (user_id),
+    FOREIGN KEY (item_id) REFERENCES items (item_id)
+);
